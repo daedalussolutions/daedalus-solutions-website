eac444e02d910c01db34a810c668d2407b28aec7
daedalussolutions <dylancassell000@hotmail.com>
2024-07-10 19:07:53 -0400

    Added footer and frontpage components. Installed routes.

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/FrontPage.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+ 
+function FrontPage() {
+    return (
+        <div className="front-page">
+            
+        </div>
+    );
+}
+
+export default FrontPage;
