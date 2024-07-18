a8ab88dda4922effc13ea7a4d628c85b7338ce11
daedalussolutions <dylancassell000@hotmail.com>
2024-07-18 13:24:30 -0400

    Added background to front page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import * as React from "react";
- 
+import logo from "../assets/images/transistor-white.png"
+
 function Home() {
     return (
         <div className="Home">
-            <h1>Home</h1>
+            <div className="home-container">
+                <div>
+                    <h1>Daedalus<br/>Solutions</h1>
+                    <h2>Professional, modern development</h2>
+                </div>
+                <div>
+                    <img src={logo}  height={600} />
+                </div>
+            </div>
         </div>
     );
 }
