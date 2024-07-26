56f35c0999990e716f217919cde0fbdfd91f4d7b
daedalussolutions <dylancassell000@hotmail.com>
2024-07-26 14:46:04 -0400

    Created contact page.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,80 @@
-import * as React from "react";
+import React, { useState } from 'react';
 
-function Contact() {
-    return (
-        <footer className="Contact">
-            <h1>Contact</h1>
-        </footer>
-    );
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission here, e.g., send the data to a server.
+    console.log(formData);
+  };
+
+  return (
+    <div className="contact-form">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <br/>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <br/>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Subject:</label>
+          <br/>
+          <input
+            type="text"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+          ></input>
+        </div>
+        <br/>
+        <div>
+          <label>Message:</label>
+          <br/>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Contact;
+export default ContactForm;
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
             <div className="home-container">
                 <div className="front-page-text">
                     <h1>Daedalus<br/>Solutions</h1>
-                    <h2>Professional, modern development</h2>
+                    <h2>Professional, modern development.</h2>
                 </div>
                 <div>
                     <img src={logo} className="front-page-logo" height={600} />
