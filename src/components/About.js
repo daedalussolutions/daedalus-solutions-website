048cc8dc04edbec37e62d9f9e0af7a12555a8855
daedalussolutions <dylancassell000@hotmail.com>
2024-08-07 16:04:22 -0400

    Added about page.

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
 import * as React from "react";
+import { SocialIcon } from 'react-social-icons'
  
 function About() {
     return (
         <div className="About">
-            <h1>About</h1>
+            <h3>
+                The goal is to provide efficient, personable development for those who seek to break away from the bloated and oversaturated technological collective consciousness.
+            </h3>
+
+            <div className='social-media-icons'>
+                <SocialIcon className='linkedin' bgColor="currentColor" fgColor="white" target="_blank" url="https://www.linkedin.com/in/dylan-cassell-" />
+                <SocialIcon className='github' bgColor="currentColor" fgColor="white" target="_blank" url="https://github.com/daedalussolutions" />
+                <SocialIcon className='twitter' bgColor="currentColor" fgColor="white" target="_blank" url="https://twitter.com/daedalussltns" />
+            </div>
+
         </div>
     );
 }
